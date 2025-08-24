2dc6a47e4943e14ee575dbce0eb453ead9762c40
andv118 <vanando110897@gmail.com>
2025-08-25 01:56:46 +0700

    update config

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -10,7 +10,8 @@ sudo yum update -y
 echo ">>> Install package..."
 
 # Cài đặt Python, pip, venv
-sudo yum install -y python3 python3-pip python3-venv
+sudo dnf install -y python3.12 python3.12-pip python3.12-venv
+sudo dnf install -y python3-pip
 
 # Cài Nginx
 sudo yum install -y nginx
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -6,8 +6,8 @@ source /home/ec2-user/www/django-crud/venv/bin/activate
 
 # migrate DB
 echo ">>> Running migrate..."
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 # collect static files
 echo ">>> Collect static..."
-python manage.py collectstatic --noinput
+# python3 manage.py collectstatic --noinput
