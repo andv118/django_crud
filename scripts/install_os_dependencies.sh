9a30b339f9786ca33b5034b40afb33f3b5027472
andv118 <vanando110897@gmail.com>
2025-08-25 01:57:19 +0700

    update config

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -10,7 +10,7 @@ sudo yum update -y
 echo ">>> Install package..."
 
 # Cài đặt Python, pip, venv
-sudo dnf install -y python3.12 python3.12-pip python3.12-venv
+sudo yum install -y python3 python3-pip python3-venv
 sudo dnf install -y python3-pip
 
 # Cài Nginx
