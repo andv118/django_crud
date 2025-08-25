8bcb004b350bc8671f30870f08085aef39d69517
andv118 <vanando110897@gmail.com>
2025-08-25 22:29:23 +0700

    update setting

--- scripts/install_gunicorn.sh
+++ scripts/install_gunicorn.sh
@@ -3,7 +3,7 @@ set -e
 
 # Copy unit file vào systemd
 echo ">>> Install gunicorn..."
-cp /home/ec2-user/www/django-crud/deploy/systemd/gunicorn.service /etc/systemd/system/gunicorn.service
+sudo cp /home/ec2-user/www/django-crud/deploy/systemd/gunicorn.service /etc/systemd/system/gunicorn.service
 
 # Reload, enable (tự bật khi reboot)
 sudo systemctl daemon-reload
